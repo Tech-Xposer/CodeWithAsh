62d0bff9ca91f5893ef76167a32af72b620a6664
Ashutosh Sharma <63233740+Tech-Xposer@users.noreply.github.com>
2023-09-14 17:15:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const {MongoClient} = require('mongodb')
-// const url = 'mongodb://localhost:27017'
-const url = 'mongodb://admin:Dev.ash%4073@3.111.168.6:27017/'
+const url = 'mongodb://localhost:27017'
+
 const client = new MongoClient(url)
 async function dbConnect(collectionName){
     let res = await client.connect()
